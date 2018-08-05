dd03175f5b5fd8f31e3153eb41f34765d63cc07c
andela-mabdussalam <mariam.abdussalam@andela.com>
2018-08-05 22:50:45 +0100

    export images

---
+++ src/assets/images.js
@@ -0,0 +1,13 @@
+const images = {
+  ban1: require('./img/1.jpg'),
+  ban2: require('./img/2.png'),
+  ban3: require('./img/3.jpg'),
+  ban4: require('./img/4.jpg'),
+  ban5: require('./img/5.jpg'),
+  ban6: require('./img/6.jpg'),
+  ban7: require('./img/7.jpg'),
+  logo: require('./img/logo.jpg'),
+};
+
+
+export default images;
